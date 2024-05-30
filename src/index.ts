f2a28756e15450efb6ce095ce666a51bb831d090
PC Win Casa <62677330+LouiisBR@users.noreply.github.com>
2024-05-30 09:36:42 -0300

    v1.2
    + add CORS

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
 import express from "express";
 import router from "./routes/index.routes";
+import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT || 3333;
+
+const corsOptions: cors.CorsOptions = {
+    origin: true, 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    allowedHeaders: ['Content-Type', 'Authorization'], 
+    credentials: true
+};
+  
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
